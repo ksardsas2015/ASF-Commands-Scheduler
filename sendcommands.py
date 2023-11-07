1062c0c7639a0a1fbc5140c6191e6f8112999397
Ksardas2015 <38585239+ksardsas2015@users.noreply.github.com>
2023-11-07 19:38:31 +0200

    Add files via upload

---
+++ sendcommands.py
@@ -0,0 +1,13 @@
+import requests
+import time
+
+def send_2fa_command():
+    url = "http://127.0.0.1:1242/Api/Command"
+    headers = {"password": "your_ipc_password"}
+    data = {"Command": "your_command"}
+    response = requests.post(url, headers=headers, json=data)
+    print(response.json())
+
+while True:
+    send_2fa_command()
+    time.sleep(20 * 60 * 60)  # 20 hours (change it)
